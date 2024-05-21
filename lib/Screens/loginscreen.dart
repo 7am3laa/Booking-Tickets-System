6f437660d84666223d36a270f2b45f994d34efb8
Mohamed Shehab <mohamedshepo280@gmail.com>
2024-05-21 21:13:07 +0300

    finishing the login

--- lib/Screens/loginscreen.dart
+++ lib/Screens/loginscreen.dart
@@ -21,154 +21,193 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ThemeData theme = Theme.of(context);
-    Color color =
-        theme.brightness == Brightness.dark ? Colors.white : Colors.black;
-    return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: BlocConsumer<UserCubit, UserState>(
-          listener: (context, state) {
-            if (state is ErrorUserState) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text(
-                    state.errorMessage,
-                    style: Styles.headlineStyle3.copyWith(color: Colors.white),
-                  ),
-                  duration: const Duration(milliseconds: 500),
+    Color txtColor = const Color.fromARGB(238, 9, 7, 98);
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Positioned(
+              top: -600,
+              right: 0,
+              child: Transform.rotate(
+                angle: 2,
+                child: Container(
+                  width: AppLayout.getWidth(context),
+                  height: AppLayout.getHeight(context),
+                  color: const Color.fromARGB(238, 9, 7, 98),
                 ),
-              );
-            } else if (state is LoadedUserState) {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SplashScreen(),
+              ),
+            ),
+            Positioned(
+              bottom: -600,
+              left: 0,
+              child: Transform.rotate(
+                angle: 2,
+                child: Container(
+                  width: AppLayout.getWidth(context),
+                  height: AppLayout.getHeight(context),
+                  color: const Color.fromARGB(238, 9, 7, 98),
                 ),
-              );
-            }
-          },
-          builder: (context, state) {
-            return Center(
-              child: Container(
-                padding: const EdgeInsets.all(20),
-                width: AppLayout.getWidth(context),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Login',
-                            style: Styles.headlineStyle1
-                                .copyWith(fontSize: 35, color: color)),
-                        Container(
-                          height: 50,
-                          width: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            image: const DecorationImage(
-                              fit: BoxFit.fitHeight,
-                              image: AssetImage('assets/images/logo2.png'),
-                            ),
-                            // color: const Color.fromARGB(238, 9, 7, 98),
+              ),
+            ),
+            Center(
+              child: Form(
+                key: _formKey,
+                child: BlocConsumer<UserCubit, UserState>(
+                  listener: (context, state) {
+                    if (state is ErrorUserState) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            state.errorMessage,
+                            style: Styles.headlineStyle3
+                                .copyWith(color: Colors.white),
                           ),
+                          duration: const Duration(milliseconds: 500),
                         ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        const Gap(20),
-                        CustomTxtField(
-                          keyboardType: TextInputType.emailAddress,
-                          validatorMessage: 'Please enter username',
-                          isobscureText: false,
-                          onSaved: (newValue) {
-                            _userName = newValue!;
-                          },
-                          labelText: 'Email or Username',
-                          suffixIcon: null,
-                          prefixIcon: Icon(
-                            Icons.mail,
-                            color: color,
-                          ),
+                      );
+                    } else if (state is LoadedUserState) {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SplashScreen(),
                         ),
-                        const Gap(10),
-                        CustomTxtField(
-                          onSaved: (newValue) {
-                            _passWord = newValue!;
-                          },
-                          keyboardType: TextInputType.visiblePassword,
-                          validatorMessage: 'Please enter password',
-                          prefixIcon: Icon(
-                            Icons.lock,
-                            color: color,
-                          ),
-                          suffixIcon: InkWell(
-                            onTap: () {
-                              context.read<UserCubit>().showPassword();
-                            },
-                            child: Icon(
-                              context.read<UserCubit>().isPasswordVisible
-                                  ? Icons.visibility_off_outlined
-                                  : Icons.remove_red_eye_outlined,
-                              color: color,
-                            ),
-                          ),
-                          isobscureText:
-                              context.read<UserCubit>().isPasswordVisible,
-                          labelText: 'Password',
-                        ),
-                        const Gap(20),
-                        Row(
+                      );
+                    }
+                  },
+                  builder: (context, state) {
+                    return Center(
+                      child: SizedBox(
+                        width: AppLayout.getWidth(context) / 1.1,
+                        child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Text('Don\'t have an account?',
-                                style: Styles.headlineStyle3
-                                    .copyWith(color: color)),
-                            InkWell(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => RegisterScreen(),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Login',
+                                  style: Styles.headlineStyle1
+                                      .copyWith(fontSize: 35, color: txtColor),
+                                ),
+                                Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    image: const DecorationImage(
+                                      fit: BoxFit.fitHeight,
+                                      image:
+                                          AssetImage('assets/images/logo2.png'),
+                                    ),
+                                    // color: const Color.fromARGB(238, 9, 7, 98),
                                   ),
-                                );
-                              },
-                              child: Text(' Register',
-                                  style: Styles.headlineStyle3.copyWith(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.blue,
-                                  )),
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                const Gap(20),
+                                CustomTxtField(
+                                  keyboardType: TextInputType.emailAddress,
+                                  validatorMessage: 'Enter username or email',
+                                  isobscureText: false,
+                                  onSaved: (newValue) {
+                                    _userName = newValue!;
+                                  },
+                                  labelText: 'Email or Username',
+                                  suffixIcon: null,
+                                  prefixIcon: Icon(
+                                    Icons.mail,
+                                    color: txtColor,
+                                  ),
+                                ),
+                                const Gap(10),
+                                CustomTxtField(
+                                  onSaved: (newValue) {
+                                    _passWord = newValue!;
+                                  },
+                                  keyboardType: TextInputType.visiblePassword,
+                                  validatorMessage: 'Enter password',
+                                  prefixIcon: Icon(
+                                    Icons.lock,
+                                    color: txtColor,
+                                  ),
+                                  suffixIcon: InkWell(
+                                    onTap: () {
+                                      context.read<UserCubit>().showPassword();
+                                    },
+                                    child: Icon(
+                                      context
+                                              .read<UserCubit>()
+                                              .isPasswordVisible
+                                          ? Icons.visibility_off_outlined
+                                          : Icons.remove_red_eye_outlined,
+                                      color: txtColor,
+                                    ),
+                                  ),
+                                  isobscureText: context
+                                      .read<UserCubit>()
+                                      .isPasswordVisible,
+                                  labelText: 'Password',
+                                ),
+                                const Gap(20),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      'Don\'t have an account?',
+                                      style: Styles.headlineStyle3
+                                          .copyWith(color: txtColor),
+                                    ),
+                                    InkWell(
+                                      onTap: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                RegisterScreen(),
+                                          ),
+                                        );
+                                      },
+                                      child: Text(
+                                        ' Register',
+                                        style: Styles.headlineStyle3.copyWith(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.blue,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const Gap(20),
+                                Button(
+                                  width: 1.5,
+                                  textColor: Colors.white,
+                                  color: const Color.fromARGB(238, 9, 7, 98),
+                                  onTap: () async {
+                                    if (_formKey.currentState!.validate()) {
+                                      _formKey.currentState!.save();
+                                      await context
+                                          .read<UserCubit>()
+                                          .getUser(_userName, _passWord);
+                                      _userName = '';
+                                      _passWord = '';
+                                    }
+                                  },
+                                  text: 'Login',
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                        const Gap(20),
-                        Button(
-                          width: 1.5,
-                          textColor: theme.brightness == Brightness.dark
-                              ? Colors.black
-                              : Colors.white,
-                          color: color,
-                          onTap: () async {
-                            if (_formKey.currentState!.validate()) {
-                              _formKey.currentState!.save();
-                              await context
-                                  .read<UserCubit>()
-                                  .getUser(_userName, _passWord);
-                              _userName = '';
-                              _passWord = '';
-                            }
-                          },
-                          text: 'Login',
-                        ),
-                      ],
-                    ),
-                  ],
+                      ),
+                    );
+                  },
                 ),
               ),
-            );
-          },
+            ),
+          ],
         ),
       ),
     );
